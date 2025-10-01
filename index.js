a9f160e61e6e710d31ae0ad9ba808f145d6dbc80
Siddharudh Sanmukh <siddusanmukh@gmail.com>
2025-10-01 18:41:07 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const path = require("path")
-
 dotenv.config()
-
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"))
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
 
 console.log("Current dirname:", __dirname)
 
